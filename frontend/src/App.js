2a3a7584fae32920ab59107ed134880042474ec5
chintuRAJamrit <133254549+chintuRAJamrit@users.noreply.github.com>
2024-05-21 19:11:58 +0530

    lil change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
+import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import Home from './Authentication/Home';
-import AdminLogin from './Authentication/AdminLogin';
-import DonorLogin from './Authentication/DonorLogin';
-import PatientLogin from './Authentication/PatientLogin';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Authentication/Home";
+import AdminLogin from "./Authentication/AdminLogin";
+import DonorLogin from "./Authentication/DonorLogin";
+import PatientLogin from "./Authentication/PatientLogin";
 //import PatientSignup from './Authentication/PatientSignup';
 //import DonorSignup from './Authentication/DonorSignup';
-
+import "./App.css";
 const Layout = () => (
   <BrowserRouter>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin</Link>
-        </li>
-        <li>
-          <Link to="/donor">Donor</Link>
-        </li>
-        <li>
-          <Link to="/patient">Patient</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin</Link>
+          </li>
+          <li>
+            <Link to="/donor">Donor</Link>
+          </li>
+          <li>
+            <Link to="/patient">Patient</Link>
+          </li>
+        </ul>
+      </nav>
 
-
-      <hr/>
       <Routes>
         <Route exact path="/" Component={Home} />
         <Route path="/admin" Component={AdminLogin} />
         <Route path="/donor" Component={DonorLogin} />
         <Route path="/patient" Component={PatientLogin} />
       </Routes>
-
     </div>
-    
   </BrowserRouter>
 );
 // function App() {
